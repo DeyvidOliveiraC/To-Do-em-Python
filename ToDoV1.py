de9eac4ddb009ad206d7ba920388896b5efe4c5a
Deyvid9959 <deyvidoliveira35@gmail.com>
2025-02-25 21:06:08 -0300

    Programa quase completo.

--- ToDoV1.py
+++ ToDoV1.py
@@ -1,37 +1,57 @@
-
+import os
 
 lista=[]
 
 def Append_Tasks(Listatarefas=[]):
 	for i in Listatarefas:
-		with open("Tarefas_Cadastradas.txt","a") as arquivo:
+		with open(f"Tarefas_Cadastradas.txt","a") as arquivo:
 			arquivo.write(f"{i}\n")
 			arquivo.close()
 
-
-
-
 def Write_Tasks():
-
 	global lista
-
 	while True:
-
 		tarefa=str(input("Digite uma tarefa a ser realizada: "))
 		lista.append(tarefa)
 		esc=str(input("Deseja cadastrar outra tarefa?[S/N]")).strip().upper()[0]
-
 		if esc=='N':
 			break
+	Append_Tasks(lista)
+	lista.clear()
 
+
+def List_Tasks():
+	global lista
+	with open("Tarefas_cadastradas.txt") as arquivo:
+		line=arquivo.readline()
+		while line:
+			lista.append(line)
+			line=arquivo.readline()
+		arquivo.close()
+
+def Show_Tasks():
+	count=1
+	with open("Tarefas_Cadastradas.txt","r") as arquivo:
+		for c in arquivo:
+			if c.isspace()==False:
+				print(f"{count}->{c}")
+				count+=1
+		arquivo.close()
+
+def Delete_Tasks(task):
+	global lista
+	List_Tasks()
+	lista.pop(task)
+	os.remove("Tarefas_Cadastradas.txt")
 	Append_Tasks(lista)
 
 def Main():
 	Write_Tasks()
+	Delete_Tasks(1)
+	Show_Tasks()
 
 
 Main()
-
 
 
 
